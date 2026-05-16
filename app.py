691dcd3c5f27a51deece7020f0131445dce30628
Kevinwuara <kevinalarcon@live.com>
2026-05-16 05:28:15 -0400

    app.py

--- app.py
+++ app.py
@@ -3347,4 +3347,6 @@ with app.app_context():
 
 # 7. SÉPTIMO ejecutar la aplicación
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
